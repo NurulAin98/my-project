ed07ef129068d0cbc23e44ab5934844ecc53c7ee
NurulAin98 <nurulainnn25@gmail.com>
2021-09-01 14:28:23 +0800

    Fetching the data from API

---
+++ src/components/random.js
@@ -0,0 +1,64 @@
+// const { useState } = require("react");
+
+// function Random() {
+//   const [title, setTitle] = useState("");
+//   const [data, setData] = useState(null);
+
+//   let dataMarkup = "There is no data";
+//   if (data !== null) {
+//     dataMarkup = data.meals.map(function (m) {
+//       return (
+//         <div>
+//           <h2>{m.strMeal}</h2>
+//           <img src={m.strMealThumb} crossOrigin="true" style={{ size: "10px 10px 10px" }}/>
+//           <h3>Area: </h3>
+//           <p>{m.strArea}</p>
+//           <h3>Instructions: </h3>
+//           <p>{m.strInstructions}</p>
+//         </div>
+//       );
+//     });
+//   }
+
+//   function updateTitle(event) {
+//     setTitle(event.target.value);
+//   }
+//   function search(event) {
+//     event.preventDefault();
+//     let url = `https://www.themealdb.com/`;
+//     let queryString = `/api/json/v1/1/search.php?s=${title}`;
+//     let httpOptions = {
+//       method: "GET",
+//     };
+//     function waitForData(response) {
+//       return response.json();
+//     }
+//     function processData(data) {
+//       console.log(data);
+//       setData(data);
+//     }
+
+//     fetch(url + queryString, httpOptions)
+//       .then(waitForData)
+//       .then(processData);
+//   }
+
+//   return (
+//     <center>
+//       <div style={{ paddingTop: "10px" }}>
+//         <form onSubmit={search}>
+//           <input
+//             type="text"
+//             placeholder="Recipe Title"
+//             value={title}
+//             onChange={updateTitle}
+//           />
+//           <input type="submit" value="Search" />
+//         </form>
+//         <div>{dataMarkup}</div>
+//       </div>
+//     </center>
+//   );
+// }
+
+// module.exports = Random;
--- src/components/recipeList.js
+++ src/components/recipeList.js
@@ -1,91 +1,22 @@
-// const { useState } = require("react");
+const { useState } = require("react");
 
-// function RecipeList() {
-//   const [title, setTitle] = useState("");
-//   const [data, setData] = useState(null);
-
-//   if (data !== null) {
-//     dataMarkup = data.meals.map(function (m) {
-//       return <div>A MOVIE</div>;
-//     });
-//   }
-
-//   function updateTitle(event) {
-//     setTitle(event.target.value);
-//   }
-//   function search(event) {
-//     event.preventDefault();
-//     console.log(title);
-//     let url = `https://www.themealdb.com/`;
-//     let queryString = `/api/json/v1/1/search.php?s=${title}`;
-
-//     let httpOptions = {
-//       method: "GET",
-//     };
-
-//     function waitForData(response) {
-//       return response.json();
-//     }
-
-//     function processData(data) {
-//       // data.meals.forEach(function (meal) {
-//       //   let title = meal.strMeal;
-//       //   let category = meal.strCategory;
-//       //   let area = meal.strArea;
-//       //   let instructions = meal.strInstructions;
-//       //   let image = meal.strMealThumb;
-//       //   //let image = meal.strMealThumb;
-//       //   let div = document.querySelector("div");
-//       //   div.innerHTML = `
-//       //       <center>
-//       //       <img src="${image}" />
-//       //       <h2>Recipe's Name: ${title}</h2>
-//       //       <h3>Category: ${category}</h3>
-//       //       <h3>Area: ${area}</h3>
-//       //       <h3>Instructions: </h3> 
-//       //       <p>${instructions}</p>
-//       //       </center>`;
-//       // });
-//       // console.log(data);
-//       setData(data);
-//     }
-
-//     fetch(url + queryString, httpOptions)
-//       .then(waitForData)
-//       .then(processData);
-//   }
-
-//   return (
-//     <center>
-//     <div>
-//       <form onSubmit={search}>
-//         <input
-//           type="text"
-//           placeholder="Recipe Title"
-//           value={title}
-//           onChange={updateTitle}
-//         />
-//         <input type="submit" value="Search" />
-//       </form>
-//       <p>{dataMarkup}</p>
-//     </div>
-//     </center>
-//   );
-// }
-
-// module.exports = RecipeList;
-
-
-import { useState } from "react";
-
-export default function RecipeList() {
+function RecipeList() {
   const [title, setTitle] = useState("");
   const [data, setData] = useState(null);
 
   let dataMarkup = "There is no data";
   if (data !== null) {
     dataMarkup = data.meals.map(function (m) {
-      return <div>A MOVIE</div>;
+      return (
+        <div>
+          <h2>{m.strMeal}</h2>
+          <img src={m.strMealThumb} crossOrigin="true" />
+          <h3>Area: </h3>
+          <p>{m.strArea}</p>
+          <h3>Instructions: </h3>
+          <p>{m.strInstructions}</p>
+        </div>
+      );
     });
   }
 
@@ -103,6 +34,7 @@ export default function RecipeList() {
       return response.json();
     }
     function processData(data) {
+      console.log(data);
       setData(data);
     }
 
@@ -113,7 +45,7 @@ export default function RecipeList() {
 
   return (
     <center>
-      <div style={{ paddingTop: "100px" }}>
+      <div style={{ paddingTop: "10px" }}>
         <form onSubmit={search}>
           <input
             type="text"
@@ -123,8 +55,10 @@ export default function RecipeList() {
           />
           <input type="submit" value="Search" />
         </form>
-        <p>{dataMarkup}</p>
+        <div>{dataMarkup}</div>
       </div>
     </center>
   );
 }
+
+module.exports = RecipeList;
