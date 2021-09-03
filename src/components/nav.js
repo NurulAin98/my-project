93efa89737ee35bc5c637256a25c3a1939cda843
NurulAin98 <nurulainnn25@gmail.com>
2021-09-03 10:57:00 +0800

    Edited the CSS

--- src/components/nav.js
+++ src/components/nav.js
@@ -6,8 +6,8 @@ function Nav({ title }) {
             <center><h1>{title}</h1></center>
             <center>
                 <nav>
-                <Link to="/">Home</Link>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <Link to="/recipeList">Find Recipe</Link>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <Link to="/">Home</Link>&nbsp;&nbsp;&nbsp;&nbsp;
+                <Link to="/recipeList">Find Recipe</Link>&nbsp;&nbsp;&nbsp;&nbsp;
                 <Link to="/random">Random Recipe</Link>
                </nav>
             </center><br></br><br></br>
